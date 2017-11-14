eccf80515944470e8afc8f90dc3acc54b7159c2f
VERANIKA <veranika.isakova89@gmail.com>
2017-11-14 21:48:25 +0100

    Update all todo counters

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ $(document).ready(function() {
 $("input[type=checkbox]").bind('change', toggleDone);
 updateCounters(); // Just add this line
 });
+function updateCounters() {
+  var todoCount = $(".todo").length;
+  var completedCount = $(".completed").length;
+
+  $("#total-count").html(todoCount);
+  $("#completed-count").html(completedCount);
+  $("#todo-count").html(todoCount - completedCount);
+}
+
+updateCounters();

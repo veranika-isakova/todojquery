db9d30d41dcffdd88715c99f88fc6e995ee3a5a0
VERANIKA <veranika.isakova89@gmail.com>
2017-11-14 22:09:13 +0100

    create todos

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function toggleDone() {
-var checkbox = this;
+  var checkbox = this;
 
-$(checkbox).parent().toggleClass("completed");
+  $(checkbox).parent().toggleClass("completed");
+  updateCounters();
 }
+
 $(document).ready(function() {
-$("input[type=checkbox]").bind('change', toggleDone);
-updateCounters(); // Just add this line
+  $("input[type=checkbox]").bind('change', toggleDone);
+  updateCounters(); // Just add this line
+  $("form").bind('submit', submitTodo);
 });
+
 function updateCounters() {
   var todoCount = $(".todo").length;
   var completedCount = $(".completed").length;
@@ -16,4 +20,45 @@ function updateCounters() {
   $("#todo-count").html(todoCount - completedCount);
 }
 
-updateCounters();
+
+function submitTodo(event) {
+  // stop the form from doing the default action, submitting...
+  event.preventDefault();
+
+  var title = $("#new-todo").val();
+
+  createTodo(title);
+
+  $("#new-todo").val(null);
+  updateCounters();
+}
+function createTodo(title) {
+  var checkboxId = "todo-" + nextTodoId();
+
+  var listItem = $("<li></li>");
+  listItem.addClass("todo");
+
+  var checkbox = $('<input>');
+  checkbox.attr('type', 'checkbox');
+  checkbox.attr('id', checkboxId);
+  checkbox.val(1);
+  checkbox.bind('change', toggleDone);
+
+  var space = document.createTextNode(" ");
+
+  var label = $('<label></label>');
+  label.attr('for', checkboxId);
+  label.html(title);
+
+  listItem.append(checkbox);
+  listItem.append(space);
+  listItem.append(label);
+
+  $("#todolist").append( listItem );
+
+  updateCounters();
+}
+
+function nextTodoId() {
+  return $(".todo").length + 1;
+}

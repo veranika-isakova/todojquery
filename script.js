2047e3c85b63e04c395cf39540f79b0cafa0e17d
VERANIKA <veranika.isakova89@gmail.com>
2017-11-14 21:47:24 +0100

    Update total todos counter

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function toggleDone() {
+var checkbox = this;
 
+$(checkbox).parent().toggleClass("completed");
+}
+$(document).ready(function() {
+$("input[type=checkbox]").bind('change', toggleDone);
+updateCounters(); // Just add this line
+});
